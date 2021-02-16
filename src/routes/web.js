4267f2ec302dde5cd5f1a03005a31661f39ecefd
yann-rzd <yann.rouzaud@gmail.com>
2021-02-16 15:02:28 +0100

    deploy heroku

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,13 @@
 const express = require('express');
+import chatBotController from '../controllers/chatBotController';
 import homepageController from '../controllers/homepageController';
 
 const router = express.Router();
 
 const initWebRoutes = (app) => {
   router.get('/', homepageController.getHomepage);
+  router.get('/webhook', chatBotController.getWebhook);
+  router.post('/webhook', chatBotController.postWebhook);
 
   return app.use('/', router);
 };
