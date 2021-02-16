95a30ed3e36bf8059a18bf66ca7ef582d4412da2
yann-rzd <yann.rouzaud@gmail.com>
2021-02-16 18:11:25 +0100

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -44,39 +44,37 @@ const getWebhook = (req, res) => {
 function handleMessage(sender_psid, received_message) {
   let response;
 
-  if (received_message.text.yo) { 
-    console.log('MSG: ', received_message.text);   
+  if (received_message.text) { 
     response = {
       "text": `You sent the message: "${received_message.text}". Now send me WESH!`
     }
   } else if (received_message.attachments) {
     let attachment_url = received_message.attachments[0].payload.url;
-      response = {
-        "attachment": {
-          "type": "template",
-          "payload": {
-            "template_type": "generic",
-            "elements": [{
-              "title": "Is this the right picture?",
-              "subtitle": "Tap a button to answer.",
-              "image_url": attachment_url,
-              "buttons": [
-                {
-                  "type": "postback",
-                  "title": "Yes!",
-                  "payload": "yes",
-                },
-                {
-                  "type": "postback",
-                  "title": "No!",
-                  "payload": "no",
-                }
-              ],
-            }]
-          }
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no",
+              }
+            ],
+          }]
         }
       }
-  
+    }
   }
   callSendAPI(sender_psid, response);
 }
