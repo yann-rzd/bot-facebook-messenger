420286bce56510accbaf7fa8e4447733a5ffd45f
yann-rzd <yann.rouzaud@gmail.com>
2021-02-16 16:27:13 +0100

    Init fb chatbot

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -1,55 +1,67 @@
 require('dotenv').config();
 
 const postWebhook = (req, res) => {
-  const body = req.body;
+  // Parse the request body from the POST
+  let body = req.body;
 
-  // Checks this is an event from a page subscription
+  // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
-    // Iterates over each entry - there may be multiple if batched
+    // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
     });
 
-    // Returns a '200 OK' response to all requests
+    // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
+
   } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
+    // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 };
 
 const getWebhook = (req, res) => {
-  // Your verify token. Should be a random string.
   const VERIFY_TOKEN = process.env.MY_VERIFY_FB_TOKEN;
     
-  // Parse the query params
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
     
-  // Checks if a token and mode is in the query string of the request
   if (mode && token) {
   
-    // Checks the mode and token sent is correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
       
-      // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
     
     } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);      
     }
   }
 };
 
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
 
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
 
 module.exports = {
   postWebhook: postWebhook,
