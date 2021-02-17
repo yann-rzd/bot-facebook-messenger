0e9a51dcb16657039851174a0be69ed663536405
yann-rzd <yann.rouzaud@gmail.com>
2021-02-17 19:18:42 +0100

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -63,7 +63,7 @@ function handleMessage(sender_psid, received_message) {
     return
   } else if (received_message.text === "Non, ça ne va pas.") {
     return
-  } else if (received_message.text == "undefined") {
+  } else if (entry.messaging[0].message.attachment[0].type === "template") {
     response = {
       "text": "Je ne sais pas traiter ce type de demande."
     }
