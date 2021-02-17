afbf2bad1f9848658d1bbe2d404ff573f4614d46
yann-rzd <yann.rouzaud@gmail.com>
2021-02-17 11:28:49 +0100

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -102,20 +102,6 @@ function callSendAPI(sender_psid, response) {
   });
 }
 
-// function firstTrait(nlp, name) {
-//   return nlp && nlp.entities && nlp.traits[name] && nlp.traits[name][0];
-// }
-
-// function handleMessage(sender_psid, message) {
-//   // check greeting is here and is confident
-//   const greeting = firstTrait(message.nlp, 'wit$greetings');
-//   if (greeting && greeting.confidence > 0.8) {
-//     callSendAPI(sender_psid, 'Hi there!');
-//   } else { 
-//     callSendAPI(sender_psid, 'Default')
-//   }
-// }
-
 module.exports = {
   postWebhook: postWebhook,
   getWebhook: getWebhook
