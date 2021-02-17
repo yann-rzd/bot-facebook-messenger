f831d551b6f6f7ba724324145ce59d5891aabe83
yann-rzd <yann.rouzaud@gmail.com>
2021-02-17 11:50:34 +0100

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -58,15 +58,7 @@ function handleMessage(sender_psid, received_message) {
 }
 
 function handlePostback(sender_psid, received_postback) {
-  let response;
-  let payload = received_postback.payload;
-
-  if (payload === 'yes') {
-    response = { "text": "Thanks!" }
-  } else if (payload === 'no') {
-    response = { "text": "Oops, try sending another image." }
-  }
-  callSendAPI(sender_psid, response);
+  
 }
 
 function callSendAPI(sender_psid, response) {
