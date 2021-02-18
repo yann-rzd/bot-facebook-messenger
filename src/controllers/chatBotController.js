08602c4371f48471cc4cc47038a7839a622cf45b
yann-rzd <yann.rouzaud@gmail.com>
2021-02-18 15:50:56 +0100

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -59,7 +59,7 @@ function handleMessage(sender_psid, received_message) {
         }
       ],
     }
-  } else if (received_message.text === "undefined") {
+  } else if (received_message.image) {
     response = {
       "text": "Je ne sais pas traiter ce type de demande."
     } 
